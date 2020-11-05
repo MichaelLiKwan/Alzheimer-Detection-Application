41ae6b3615911de7affae96ce9f6b6549a4804bf
jinwen-loh482 <jwl482@nyu.edu>
2020-11-04 20:20:52 -0500

    temp

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 conn = pymysql.connect(host='localhost',
                        port=3306,
                        user='root',
-                       password='',
+                       password='root',
                        db='alzheimersPatients',
                        charset='utf8mb4',
                        cursorclass=pymysql.cursors.DictCursor)
